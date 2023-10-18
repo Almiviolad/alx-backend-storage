5d0732226849a825d7fd14a0793616f0e5954ac6
Aloh Michael <mykelaloh@gmal.com>
2023-10-17 19:59:47 -0700

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,5 +6,4 @@ def update_topics(mongo_collection, name, topics):
     of a school document based on the name:"""
     search = {"name": name}
     new_values = {"$set": {"topics": topics}}
-    x = mongo_collection.update_many(search, new_values)
-    print (x)
+    mongo_collection.update_many(search, new_values)
