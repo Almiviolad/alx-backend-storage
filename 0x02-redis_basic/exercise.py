098baf1898a06d98fb0d565e641ac5eb1a566a92
Aloh Michael <mykelaloh@gmail.com>
2023-10-19 04:59:51 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,7 +8,7 @@ Create a store method that takes a data argument and returns a string.
 The method should generate a random key (e.g. using uuid),
 store the input data in Redis using the random key and return the key."""
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 import redis
 
 
@@ -22,8 +22,28 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store data in redus db"""
-        id = uuid.uuid4()
-        key = str(id)
-        self._redis.set(key, data)
+        self.id = uuid.uuid4()
+        self.key = str(self.id)
+        self._redis.set(self.key, data)
         self._redis.close()
-        return key
+        return self.key
+    
+    def get_str(self,byte_result: bytes) -> str:
+        """converts byte to str"""
+        return byte_result.decode('utf-8')
+
+    def get_int(self, byte_result: bytes) -> int:
+        """ converts byte to int"""
+        return int(byte_result.decode('utf-8'))
+
+    def get(self, key: str, fn: Optional[Callable]) -> bytes:
+        """getts vlue using key frim refis db"""
+        value = self._redis.get(key)
+        print(fn)
+        if (value):
+            print(value)
+            if (str(fn) == "<class 'str'>"):
+                return self.get_str(value)
+            elif (str(fn) == "<class 'int'>"):
+                return self.get_int(value)
+        return value
