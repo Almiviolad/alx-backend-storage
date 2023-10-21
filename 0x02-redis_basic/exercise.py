3aeb6c120dec82d16c764d8f0216284491bc9423
Aloh Michael <mykelaloh@gmail.com>
2023-10-21 07:09:18 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,23 +27,18 @@ class Cache:
         self._redis.set(self.key, data)
         self._redis.close()
         return self.key
-    
-    def get_str(self,byte_result: bytes) -> str:
-        """converts byte to str"""
-        return byte_result.decode('utf-8')
 
-    def get_int(self, byte_result: bytes) -> int:
-        """ converts byte to int"""
-        return int(byte_result.decode('utf-8'))
-
-    def get(self, key: str, fn: Optional[Callable]) -> bytes:
-        """getts vlue using key frim refis db"""
+    def get(self, key: str, fn: Optional[Callable]=None) -> Union[str, bytes, int, float]:
+        """gets value from redis db using key, fn changes it to fotmat"""
         value = self._redis.get(key)
-        print(fn)
-        if (value):
-            print(value)
-            if (str(fn) == "<class 'str'>"):
-                return self.get_str(value)
-            elif (str(fn) == "<class 'int'>"):
-                return self.get_int(value)
+        if (fn):
+            return fn(value)
         return value
+
+    def get_str(self, key: bytes) -> str:
+        """converts byte to str"""
+        return self.get(key, fn=lambda v: v.decode('utf-8'))
+
+    def get_int(self, key: bytes) -> int:
+        """ converts byte to int"""
+        return self.get(key, fn=int)
